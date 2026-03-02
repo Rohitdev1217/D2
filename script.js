d3da78e2229e86a9db636fdab89fb0f25b937c97
Rohitdev1217 <sonirohitrm@gmail.com>
2026-03-02 17:20:21 +0530

    Add array manipulation examples to script.js

--- script.js
+++ script.js
@@ -25,3 +25,12 @@ console.log(jsonobj2);
 
 let convertJson = JSON.parse(jsonobj2);
 console.log(convertJson);
+
+let arr = [1, 2, 3, 4, 5];
+
+let arr1 = [...arr];
+
+arr1.pop();
+
+if (12 > 13) console.log("12 is greater than 13");
+else console.log("12 is not greather than 13");
