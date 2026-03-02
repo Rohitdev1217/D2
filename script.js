149ec787bfd92718f1c4150e7530f74af628d140
Rohitdev1217 <sonirohitrm@gmail.com>
2026-03-02 16:52:05 +0530

    Add JSON parsing and stringifying examples to script.js

--- script.js
+++ script.js
@@ -5,5 +5,23 @@ let obj = {
 };
 
 let jsonString = JSON.stringify(obj);
-
 console.log(jsonString);
+
+// how to convert json string back to object .JSON
+
+let jsonString1 = jsonString;
+
+let obj1 = JSON.parse(jsonString1);
+console.log(obj);
+
+let obj2 = {
+  name: "heyehy",
+  age: 21,
+  email: "vishu@hey.com",
+};
+
+let jsonobj2 = JSON.stringify(obj2);
+console.log(jsonobj2);
+
+let convertJson = JSON.parse(jsonobj2);
+console.log(convertJson);
