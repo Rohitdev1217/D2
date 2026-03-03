ebfa0dd1786386e1eeba1d796789fbebcce6f353
Rohitdev1217 <sonirohitrm@gmail.com>
2026-03-03 13:38:24 +0530

    Comment out example code in script.js for clarity

--- script.js
+++ script.js
@@ -1,36 +1,62 @@
-let obj = {
-  name: "john",
-  age: 30,
-  city: "New York",
-};
+// let obj = {
+//   name: "john",
+//   age: 30,
+//   city: "New York",
+// };
 
-let jsonString = JSON.stringify(obj);
-console.log(jsonString);
+// let jsonString = JSON.stringify(obj);
+// console.log(jsonString);
 
-// how to convert json string back to object .JSON
+// // how to convert json string back to object .JSON
 
-let jsonString1 = jsonString;
+// let jsonString1 = jsonString;
 
-let obj1 = JSON.parse(jsonString1);
-console.log(obj);
+// let obj1 = JSON.parse(jsonString1);
+// console.log(obj);
 
-let obj2 = {
-  name: "heyehy",
-  age: 21,
-  email: "vishu@hey.com",
-};
+// let obj2 = {
+//   name: "heyehy",
+//   age: 21,
+//   email: "vishu@hey.com",
+// };
 
-let jsonobj2 = JSON.stringify(obj2);
-console.log(jsonobj2);
+// let jsonobj2 = JSON.stringify(obj2);
+// console.log(jsonobj2);
 
-let convertJson = JSON.parse(jsonobj2);
-console.log(convertJson);
+// let convertJson = JSON.parse(jsonobj2);
+// console.log(convertJson);
 
-let arr = [1, 2, 3, 4, 5];
+// let arr = [1, 2, 3, 4, 5];
 
-let arr1 = [...arr];
+// let arr1 = [...arr];
 
-arr1.pop();
+// arr1.pop();
 
-if (12 > 13) console.log("12 is greater than 13");
-else console.log("12 is not greather than 13");
+// if (12 > 13) console.log("12 is greater than 13");
+// else console.log("12 is not greather than 13");
+
+// // for (let i = 0; i <= 11; i++) {
+// //   console.log(i);
+// // }
+
+// let i = 1;
+// while (i <= 5) {
+//   console.log(i);
+//   i++;
+// }
+
+// let j = 10;
+// do {
+//   console.log(j);
+//   j++;
+// } while (j <= 5);
+
+// let map = new Map();
+// map.set("name", "john");
+// map.set("age", 30);
+// map.set("city", "new york");
+
+// console.log(map.get("age"));
+
+let set = new Set([1, 2, 2, 3, 4, 5]);
+console.log(set);
